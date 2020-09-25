9bba2ca963088b904e00abd5077a97f77479e89a
HARIKIRAN MADISHETTI <madishetti@HARIKIRANs-MacBook-Pro.local>
2020-09-25 13:12:29 -0500

    Creating JWT and integrating it in users js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const bcrypt = require('bcryptjs');
@@ -41,7 +43,18 @@ router.post('/', [
 
         await user.save();
 
-        res.send('User Saved')
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+        });
 
     } catch (err) {
         console.error(err.message);
