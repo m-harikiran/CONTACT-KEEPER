2f7be3271bbb4d2197651178a4bd462b4f807b1f
HARIKIRAN MADISHETTI <madishetti@HARIKIRANs-MacBook-Pro.local>
2020-09-25 13:10:17 -0500

    Hashing the user password using bcryptjs and saving user model in db

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../models/User');
 const { check, validationResult } = require('express-validator');
 
+const bcrypt = require('bcryptjs');
+
 //@route    POST api/users
 // @desc    Register a user
 // @access  Public
@@ -13,13 +15,38 @@ router.post('/', [
     check('password', 'Please enter a password with 6 or more characters').isLength({
         min: 6
     })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed')
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' });
+        }
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('User Saved')
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
 });
 
 module.exports = router;
