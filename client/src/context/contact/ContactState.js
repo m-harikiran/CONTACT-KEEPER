c2ac27a56d50046fc724bca6e1f9cbe71c0dfe37
HARIKIRAN MADISHETTI <madishetti@HARIKIRANs-MacBook-Pro.local>
2020-09-26 01:52:51 -0500

    ContactForm Component

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -11,7 +11,7 @@ import {
     FILTER_CONTACTS,
     CLEAR_FILTER
 } from '../types';
-import contactContext from './contactContext';
+
 
 const ContactState = props => {
     const initialState = {
@@ -43,6 +43,10 @@ const ContactState = props => {
     const [state, dispatch] = useReducer(contactReducer, initialState);
 
     // Add Contact
+    const addContact = contact => {
+        contact.id = uuid();
+        dispatch({ type: ADD_CONTACT, payload: contact })
+    }
 
     // Delete Contact
 
@@ -57,12 +61,13 @@ const ContactState = props => {
     // Clear Filter
 
     return (
-        <contactContext.Provider
+        <ContactContext.Provider
             value={{
-                contacts: state.contacts
+                contacts: state.contacts,
+                addContact
             }}>
             {props.children}
-        </contactContext.Provider>
+        </ContactContext.Provider>
     )
 }
 
