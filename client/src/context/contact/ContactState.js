44ad441b08291f72be500e2f6213b317feccfe99
HARIKIRAN MADISHETTI <madishetti@HARIKIRANs-MacBook-Pro.local>
2020-09-26 03:14:11 -0500

    Filtering contacts and showing in contact items

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,7 +38,8 @@ const ContactState = props => {
                 type: 'professional'
             }
         ],
-        current: null
+        current: null,
+        filtered: null
     };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -68,19 +69,28 @@ const ContactState = props => {
         dispatch({ type: UPDATE_CONTACT, payload: contact })
     };
     // Filter Contacts
+    const filterContacts = text => {
+        dispatch({ type: FILTER_CONTACTS, payload: text })
+    };
 
     // Clear Filter
+    const clearFilter = () => {
+        dispatch({ type: CLEAR_FILTER })
+    };
 
     return (
         <ContactContext.Provider
             value={{
                 contacts: state.contacts,
                 current: state.current,
+                filtered: state.filtered,
                 addContact,
                 deleteContact,
                 setCurrent,
                 clearCurrent,
-                updateContact
+                updateContact,
+                clearFilter,
+                filterContacts
             }}>
             {props.children}
         </ContactContext.Provider>
