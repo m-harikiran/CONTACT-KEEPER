da724d1ef2f8144cc1fe768e0aa807f2db4b7e14
HARIKIRAN MADISHETTI <madishetti@HARIKIRANs-MacBook-Pro.local>
2020-09-26 02:20:00 -0500

    Setting up the current for editing the form

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -5,10 +5,10 @@ import ContactContext from '../../context/contact/contactContext';
 
 const ContactItem = ({ contact }) => {
     const contactContext = useContext(ContactContext);
-    const { deleteContact } = contactContext;
+    const { deleteContact, setCurrent, clearCurrent } = contactContext;
     const { id, name, email, phone, type } = contact;
 
-    const onDelete = () => { deleteContact(id) }
+    const onDelete = () => { deleteContact(id); clearCurrent() }
 
     return (
         <div className='card bg-light'>
@@ -27,7 +27,7 @@ const ContactItem = ({ contact }) => {
                 </li>)}
             </ul>
             <p>
-                <button className='btn btn-dark btn-sm'>Edit</button>
+                <button className='btn btn-dark btn-sm' onClick={() => setCurrent(contact)} >Edit</button>
                 <button className='btn btn-danger btn-sm' onClick={onDelete}>Delete</button>
             </p>
 
