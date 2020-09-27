4925ecc1fb241a6f0a666de83caf6b9b0dcb7719
HARIKIRAN MADISHETTI <madishetti@HARIKIRANs-MacBook-Pro.local>
2020-09-27 02:24:07 -0500

    Final deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path')
 
 const app = express();
 
@@ -18,6 +19,13 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
+// Serv Static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set Static Folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
